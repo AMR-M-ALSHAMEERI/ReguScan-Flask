9cef71185b083de7da4eef404ba8982cab60dd8e
AMR-M-ALSHAMEERI <180756848+AMR-M-ALSHAMEERI@users.noreply.github.com>
2026-01-29 22:19:39 +0800

    feat: Add Procfile for deployment and update app configuration for dynamic port assignment

--- app.py
+++ app.py
@@ -401,4 +401,5 @@ def request_entity_too_large(error):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
